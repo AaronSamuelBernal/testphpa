c7c855e0d2d52488d4d57b66a8e69a6789eb0288
AaronSamuelBernal <samu.aaron@hotmail.com>
2021-09-07 17:54:07 -0300

    prueba

---
+++ index.php
@@ -0,0 +1,14 @@
+<html>
+ <head>
+  <title>Prueba de PHP</title>
+ </head>
+ <body>
+ <?php echo '<p>Hola Mundo</p>'; ?>
+ </body>
+ <body>
+     <div style="font-size : 40px; text-align: center;">
+        <p>Deploy OK </p>
+        <img width = "350px" src= "./image.gif" />
+    </div>
+ </body>
+</html>
